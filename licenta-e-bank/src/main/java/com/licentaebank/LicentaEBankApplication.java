63fc5669922608c423826e3cd4ff1291cb7741f8
Dan Ghita <mateidanghita@gmail.com>
2024-04-21 20:34:11 +0300

    Added E-Banking-App-database.sql

---
+++ licenta-e-bank/src/main/java/com/licentaebank/LicentaEBankApplication.java
@@ -0,0 +1,13 @@
+package com.licentaebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicentaEBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicentaEBankApplication.class, args);
+	}
+
+}
---
+++ licenta-e-bank/src/test/java/com/licentaebank/LicentaEBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licentaebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicentaEBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
