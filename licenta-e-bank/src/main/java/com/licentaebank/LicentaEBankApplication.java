78012ca5941c6eda47eb9207f5ea480b4088bb40
Dan Ghita <mateidanghita@gmail.com>
2024-04-21 21:57:03 +0300

    Added but haven't finished Spring

--- licenta-e-bank/src/main/java/com/licentaebank/LicentaEBankApplication.java
+++ licenta-e-bank/src/main/java/com/licentaebank/LicentaEBankApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LicentaEBankApplication {
+public class LicentaEBankApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicentaEBankApplication.class, args);
