871e485606cc15b113ebf50c4a4e3e2b70584453
Dan Ghita <mateidanghita@gmail.com>
2024-04-22 13:27:59 +0300

    WIP - Added index.jsp

---
+++ licenta-e-bank/src/main/java/com/licentaebank/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.licentaebank.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView getIndex(){
+        ModelAndView getIndexPage = new ModelAndView("index");
+        getIndexPage.addObject("PageTitle", "Home");
+        System.out.println("In index Controller");
+        return getIndexPage;
+    }
+}
